316225705e050ab1dc4cf3d51241a01489af9d5c
adityamwagh <adityamwagh@outlook.com>
2026-05-26 01:59:44 +0100

    fix(nvim/treesitter): fix config for post-rewrite API
    nvim-treesitter removed the configs submodule; setup now goes through
    the top-level module. Also disable lazy-loading which the rewrite
    explicitly does not support.

--- editors/nvim/lua/plugins/treesitter.lua
+++ editors/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,13 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-        ensure_installed = {
-            "lua",
-            "vim",
-            "vimdoc",
-            "query",
-        },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-    },
-    main = "nvim-treesitter",
+    lazy = false,
+    config = function()
+        require("nvim-treesitter").setup({
+            ensure_installed = { "lua", "vim", "vimdoc", "query" },
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
 }
